c719f69bef275d567f0cac13bc467f9d6e2409be
xenamorphh <77855011+xenamorphh@users.noreply.github.com>
2021-03-26 11:16:54 -0400

    Week11

---
+++ week11/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Card, Button} from 'react-native-elements';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     <Card>
+       <Card.Title> Kayla Meguin</Card.Title>
+       <Text>Sushi</Text>
+       <Card.Divider/>
+       <Text>Pizza</Text>
+       <Button title="Food"/>
+     </Card>     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
